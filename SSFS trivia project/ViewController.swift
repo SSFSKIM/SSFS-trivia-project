54a0b9df1c080502f9b1507d7a0ea9d3ee46580c
SSFSKIM <kimmi@ssfs.org>
2024-04-18 11:15:36 -0400

    Initial Commit

---
+++ SSFS trivia project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSFS trivia project
+//
+//  Created by Steve on 4/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
